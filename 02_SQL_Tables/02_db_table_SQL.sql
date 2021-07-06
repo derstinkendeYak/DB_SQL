1853705d97de8d0eca2975fd5eeaad19fa49ff80
przygoda <schmidt.multimedia@googlemail.com>
2021-07-06 08:22:45 +0000

    Update

--- 02_SQL_Tables/02_db_table_SQL.sql
+++ 02_SQL_Tables/02_db_table_SQL.sql
@@ -2,3 +2,40 @@
 
 /* Datenbanken auf Server anzeigen */
 SHOW DATABASES;
+
+/* DB boo löschen, falls vorhanden*/
+DROP DATABASE IF EXISTS boo;
+
+/* DB boo anlegen, falls noch nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
+
+/* DB auswählen */
+USE boo;
+
+/* Tabelle anlegen, falls noch nicht vorhanden */
+CREATE TABLE IF NOT EXISTS test
+(
+    name VARCHAR(20),
+    age INT
+);
+
+SHOW TABLES;
+
+/* Struktur der Tabelle anzeigen */
+DESCRIBE test;
+
+/* --------  Daten ------------ */
+
+INSERT INTO test(name,age) VALUES ("Grizabella",29);
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
+INSERT INTO test VALUES ();
+
+/* -- Tabelleninhalte anzeigen -- */
+SELECT * FROM test;
+
+/* Tabelle test löschen, falls vorhanden*/
+DROP TABLE IF EXISTS test;
+
+SHOW TABLES;
+
+
