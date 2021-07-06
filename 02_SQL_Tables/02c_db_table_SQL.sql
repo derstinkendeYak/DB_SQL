8d12f32107e79f9163bdce28a3b3e291cdb82dc5
przygoda <schmidt.multimedia@googlemail.com>
2021-07-06 10:35:39 +0000

    update 02c / 2. Variante  PRIMARY KEY + AUTO_INCREMENT

--- 02_SQL_Tables/02c_db_table_SQL.sql
+++ 02_SQL_Tables/02c_db_table_SQL.sql
@@ -15,13 +15,14 @@ DROP TABLE IF EXISTS boo.test;
 
 /* 	 
 	Tabelle mit id als PRIMARY KEY --> KEINE Duplikate moeglich
+    AUTO_INCREMENT ++
     DEFAULT-Werte eintragen
 */
 
 /* Tabelle anlegen, falls noch nicht vorhanden */
 CREATE TABLE IF NOT EXISTS boo.test
 (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL DEFAULT "TBA",
     age INT NOT NULL DEFAULT 0
 );
@@ -30,14 +31,11 @@ CREATE TABLE IF NOT EXISTS boo.test
 DESCRIBE test;
 
 /* --------  Daten ------------ */
-INSERT INTO test(id,name,age) VALUES (1,"Grizabella",29);
-INSERT INTO test(id,age,name) VALUES (2,35,"Alonzo");
-INSERT INTO test(id,age,name) VALUES (3,35,"Alonzo");
-#INSERT INTO test(id,age,name) VALUES (4);
-
-# Doppelte Datensätze werden zugelassen !
-#INSERT INTO test(age,name) VALUES (35,"Big Alonzo");
-
+INSERT INTO test(name,age) VALUES ("Grizabella",29);
+INSERT INTO test(name,age) VALUES ("Gus",45);
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
+INSERT INTO test VALUES ();
 
 /* -- Tabelleninhalte anzeigen -- */
 SELECT * FROM test;
