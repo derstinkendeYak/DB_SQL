2163a58e32d4c4e6bcd1539648739ed125d17896
przygoda <schmidt.multimedia@googlemail.com>
2021-07-07 12:35:52 +0000

    update

--- 04_SQL_DataImport/04a_db_csv_import_SQL.sql
+++ 04_SQL_DataImport/04a_db_csv_import_SQL.sql
@@ -10,20 +10,21 @@ DROP TABLE IF EXISTS cats;
 
 CREATE TABLE IF NOT EXISTS cats
 (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     cat_name VARCHAR(20) NOT NULL,
     fur_color VARCHAR(20) NOT NULL,
-    age INT NOT NULL
+    cat_age INT NOT NULL
 );
 
 #DESCRIBE cats;
 
 /* CSV-Date parsen */
-LOAD DATA LOCAL INFILE "04_SQL_DataImport/data/cats_export.csv" 
+LOAD DATA LOCAL INFILE "04_SQL_DataImport/data/cats_export_no_id.csv" 
 INTO TABLE cats
-FIELDS TERMINATED BY ";"
+FIELDS TERMINATED BY ","
 LINES TERMINATED BY "\n"
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
+(cat_name,fur_color,cat_age);
 
 
 SELECT * FROM cats;
