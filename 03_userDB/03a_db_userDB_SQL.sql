e9c04bdb4a98576f36d5148098041a8d26dfd7c9
przygoda <schmidt.multimedia@googlemail.com>
2021-07-07 08:42:57 +0000

    update 03/2 : userDB mit UNIQUE + SHA1() / pwd

---
+++ 03_userDB/03a_db_userDB_SQL.sql
@@ -0,0 +1,34 @@
+/* USER DB Vers. 1 */
+
+/* DB */
+DROP DATABASE IF EXISTS boo;
+CREATE DATABASE IF NOT EXISTS boo;
+USE boo;
+
+/* Tabelle users*/
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(20) UNIQUE NOT NULL,
+    user_pwd VARCHAR(40) NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    family_name VARCHAR(20) NOT NULL
+);
+
+DESCRIBE users;
+
+/* Daten  - Nachteil: PWD offen lesbar!! 
+INSERT INTO users(user_name,user_pwd,first_name,family_name) VALUES ("max","1234","Max","Mütze");
+INSERT INTO users(user_name,user_pwd,first_name,family_name) VALUES ("maxine","#7Dx0","Maxine","Mütze");
+INSERT INTO users(user_name,user_pwd,first_name,family_name) VALUES ("max2","user1234","Max","Mützerich");
+*/
+
+/* Daten  - Besser: PWD verschlüsselt!*/
+INSERT INTO users(user_name,user_pwd,first_name,family_name) VALUES ("max",sha1("1234"),"Max","Mütze");
+INSERT INTO users(user_name,user_pwd,first_name,family_name) VALUES ("maxine",sha1("#7Dx0"),"Maxine","Mütze");
+INSERT INTO users(user_name,user_pwd,first_name,family_name) VALUES ("max2",sha1("user1234"),"Max","Mützerich");
+
+SELECT * FROM users;
