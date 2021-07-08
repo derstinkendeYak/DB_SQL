d3817941e438e92d6095a06226c403db9f0457de
przygoda <schmidt.multimedia@googlemail.com>
2021-07-08 07:32:02 +0000

    update 04 / UPDATE / SELECT

---
+++ 04_SQL_DataImport/04b_db_CRUD.sql
@@ -0,0 +1,33 @@
+/* CRUD Create | Read | Update | Delete */  
+
+# CREATE --> siehe 04a ....
+
+# UPDATE --> Änderung in bestehender Tab.
+
+#UPDATE cats SET cat_name = "Alonzo"; # Achtung, gesamte Spalte wird überschrieben!
+#UPDATE cats SET cat_name = "Alonzo" WHERE id = 2; #Besser!
+#UPDATE cats SET cat_name = "Big Alonzo" WHERE fur_color = "grey";
+#SELECT * FROM cats;
+
+/* READ - SELECT : NEUE Ergebnistabelle wird erstellt */
+SELECT * FROM cats; #gesamte Tabelle
+SELECT cat_name FROM cats; #Feld
+SELECT cat_name,age FROM cats; #2 Felder kombiniert
+SELECT age,cat_name FROM cats; #2 Felder kombiniert
+SELECT cat_name AS "Name" , age AS "Alter" FROM cats; #2 Felder / neue Namen
+
+/*Restriktionen durch WHERE*/
+SELECT 
+	cat_name AS "Name", 
+	age AS "Alter" 
+FROM cats
+#WHERE id = 1
+#WHERE fur_color = "red"
+WHERE age > 35
+;
+
+/* Delete - Vorsicht!! */
+DELETE FROM cats;  # Alle Daten sind weg!!
+SELECT * FROM cats; #gesamte Tabelle
+
+DESCRIBE cats;
