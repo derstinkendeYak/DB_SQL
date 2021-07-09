55365795133ad6b39f9edc3405700c41bf0b4a58
przygoda <schmidt.multimedia@googlemail.com>
2021-07-09 10:15:06 +0000

    update 05_6b/ COUNT() / agg. + nicht agg. / GROUP BY

--- 05_SQL_SELECT/05_6b_que_count.sql
+++ 05_SQL_SELECT/05_6b_que_count.sql
@@ -9,6 +9,7 @@ USE boo;
 */
 
 -- Welche VERSCHIEDENEN Sektoren ?
+/*
 SELECT
     DISTINCT sector Industriesektoren
 FROM ccc_list
@@ -17,14 +18,33 @@ ORDER BY sector ASC
 
 -- Wieviele VERSCHIEDENE Sektoren ?
 SELECT
-    COUNT(DISTINCT sector) "Anzahl Sektoren"
+    COUNT(DISTINCT sector) Sektorenanzahl
 FROM ccc_list
 ;
+*/
 
+-- Welche VERSCHIEDENEN Branchen ?
+/*
+SELECT
+    DISTINCT industry Branchen
+FROM ccc_list
+ORDER BY industry ASC
+;
 
+-- Wieviele VERSCHIEDENE Branchen ?
+SELECT
+    COUNT(DISTINCT industry) Branchenzahl
+FROM ccc_list
+;
+*/
 
-
-
-
-
+-- Wieviele VERSCHIEDENE Branchen gibt es 
+-- in den jeweiligen Industriesektoren?
+SELECT
+    sector Industriesektoren, # NICHT aggregiert --> organisch
+    COUNT(DISTINCT industry) Branchen #aggregiert --> durch Fkt. entstanden
+FROM ccc_list
+GROUP BY sector # bei Kombination nicht aggr. /aggr.
+ORDER BY Branchen DESC
+;
 
